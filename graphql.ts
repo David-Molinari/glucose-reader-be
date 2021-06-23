cd86ee6d9776b2a55e098ccae6a1857f8c08a002
David Molinari <david.m.molinari@gmail.com>
2021-06-23 13:33:01 -0400

    v1

--- graphql.ts
+++ graphql.ts
@@ -2,8 +2,6 @@ import { ApolloServer } from "apollo-server";
 import { resolvers } from "./resolvers/index";
 import { schema } from "./schemas/index";
 import { context } from "./context";
-import { AuthenticationError } from 'apollo-server-errors';
-
 
 /**
  * @description holds and creates apollo server
@@ -12,19 +10,6 @@ import { AuthenticationError } from 'apollo-server-errors';
 const apolloServer = new ApolloServer({
   typeDefs: schema,
   resolvers,
-  plugins: [
-    {
-      requestDidStart() {
-        return {
-          didEncounterErrors({ response, errors }) {
-            if (errors.find(err => err instanceof AuthenticationError)) {
-              console.log('401 ERROR')
-            }
-          }
-        }
-      },
-    },
-  ],
   context: context,
   playground: {
     endpoint: "/graphql"
